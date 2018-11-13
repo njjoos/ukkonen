97b3511b59e3e2fef278766a26fef75eb5677f3a
njjoos <nathaniel.joos@ugent.be>
2018-11-13 18:57:06 +0100

    minimal testing done.

--- tests/ukkonen/main.c
+++ tests/ukkonen/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <memory.h>
+#include <stdlib.h>
 #include "../../src/ukkonen/ukkonen.h"
 #include "../util.h"
 
 // The amount of outgoing edges from root must be equal to the amount of unique characters in the string
-int test_root_outgoing_edges(node* root, const char* string) {
+void test_root_outgoing_edges(node* root, const char* string) {
+
     int amount   = unique_chars(string);
     int c_amount = 0;
 
@@ -17,18 +19,71 @@ int test_root_outgoing_edges(node* root, const char* string) {
 
     }
 
-    return amount == c_amount ? 1 : 0;
+    printf(" Test result of outgoing edges from root:\n");
+    printf("  Unique characters = %d \n", amount);
+    printf("  Outgoing edges    = %d \n", c_amount);
+
+    if (amount == c_amount)
+        printf("  Test passed.\n");
+    else
+        printf("  Test failed.\n");
+
 }
 
+void count_leaf_nodes_helper(node *n, int *counter) {
+
+    if (!n->outgoing_edges)
+        (*counter)++;
+    else {
+        for (int i = 0; i < ASCII_LENGTH; i++) {
+            edge* e = n->outgoing_edges[i];
+            if (e != 0)
+                count_leaf_nodes_helper(e->end_node, counter);
+        }
+    }
+}
+
+int count_leaf_nodes(node* root) {
+
+    int* counter = malloc(sizeof(int));
+    *counter = 0;
+    count_leaf_nodes_helper(root, counter);
+
+    return *counter;
+}
+
+
 // The amount of leaf nodes must be equals to the length of the string
-int test_leaf_nodes(node* root, const char* string) {
-    // TODO
+void test_leaf_nodes(node* root, const char* string) {
+
+    int leaf_nodes = count_leaf_nodes(root);
+    int characters = (int) strlen(string);
+
+    printf(" Test result of leaf nodes:\n");
+    printf("  Amount of leaf nodes = %d \n", leaf_nodes);
+    printf("  Length of the string = %d \n", characters);
+
+    if (leaf_nodes == characters)
+        printf("  Test passed.\n\n");
+    else
+        printf("  Test failed.\n\n");
+
+}
+
+void test(char* string) {
+    node* root = create_suffix_tree(string);
+
+    printf("Testing the string: %s \n\n", string);
+    print_suffix_tree(root);
+    printf("\n");
+    test_root_outgoing_edges(root, string);
+    test_leaf_nodes(root, string);
 }
 
 int main() {
 
-    char* gg = "putainkerlkemassashonger$";
-    node* root = create_suffix_tree(gg);
-    test_root_outgoing_edges(root, gg);
+    test("abacabc_");
+    test("eatheaheearththeearthw_");
+
     return 0;
 }
